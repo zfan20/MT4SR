4e695a4a4dc7c05d96ffc24f47011e814bdde4ea
zfan20 <zfan20@uic.edu>
2022-02-09 19:14:16 -0600

    init commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # RSASRec
+code submission of RSASRec for KDD 2022
---
+++ seqmodels.py
@@ -0,0 +1,206 @@
+import torch
+import torch.nn as nn
+from modules import Encoder, LayerNorm, DistSAEncoder, DistMeanSAEncoder, RelationAwareSAEncoder
+
+class SASRecModel(nn.Module):
+    def __init__(self, args):
+        super(SASRecModel, self).__init__()
+        self.item_embeddings = nn.Embedding(args.item_size, args.hidden_size, padding_idx=0)
+        self.position_embeddings = nn.Embedding(args.max_seq_length, args.hidden_size)
+        self.item_encoder = Encoder(args)
+        self.LayerNorm = LayerNorm(args.hidden_size, eps=1e-12)
+        self.dropout = nn.Dropout(args.hidden_dropout_prob)
+        self.args = args
+
+        self.criterion = nn.BCELoss(reduction='none')
+        self.apply(self.init_weights)
+
+    def add_position_embedding(self, sequence):
+
+        seq_length = sequence.size(1)
+        position_ids = torch.arange(seq_length, dtype=torch.long, device=sequence.device)
+        position_ids = position_ids.unsqueeze(0).expand_as(sequence)
+        item_embeddings = self.item_embeddings(sequence)
+        position_embeddings = self.position_embeddings(position_ids)
+        sequence_emb = item_embeddings + position_embeddings
+        sequence_emb = self.LayerNorm(sequence_emb)
+        sequence_emb = self.dropout(sequence_emb)
+
+        return sequence_emb
+
+
+    def finetune(self, input_ids):
+
+        attention_mask = (input_ids > 0).long()
+        extended_attention_mask = attention_mask.unsqueeze(1).unsqueeze(2) # torch.int64
+        max_len = attention_mask.size(-1)
+        attn_shape = (1, max_len, max_len)
+        subsequent_mask = torch.triu(torch.ones(attn_shape), diagonal=1) # torch.uint8
+        subsequent_mask = (subsequent_mask == 0).unsqueeze(1)
+        subsequent_mask = subsequent_mask.long()
+
+        if self.args.cuda_condition:
+            subsequent_mask = subsequent_mask.cuda()
+
+        extended_attention_mask = extended_attention_mask * subsequent_mask
+        extended_attention_mask = extended_attention_mask.to(dtype=next(self.parameters()).dtype) # fp16 compatibility
+        extended_attention_mask = (1.0 - extended_attention_mask) * -10000.0
+
+        sequence_emb = self.add_position_embedding(input_ids)
+
+        item_encoded_layers = self.item_encoder(sequence_emb,
+                                                extended_attention_mask,
+                                                output_all_encoded_layers=True)
+
+        sequence_output = item_encoded_layers[-1]
+        return sequence_output
+
+    def init_weights(self, module):
+        """ Initialize the weights.
+        """
+        if isinstance(module, (nn.Linear, nn.Embedding)):
+            # Slightly different from the TF version which uses truncated_normal for initialization
+            # cf https://github.com/pytorch/pytorch/pull/5617
+            module.weight.data.normal_(mean=0.0, std=self.args.initializer_range)
+        elif isinstance(module, LayerNorm):
+            module.bias.data.zero_()
+            module.weight.data.fill_(1.0)
+        if isinstance(module, nn.Linear) and module.bias is not None:
+            module.bias.data.zero_()
+
+
+class DistSAModel(nn.Module):
+    def __init__(self, args):
+        super(DistSAModel, self).__init__()
+        self.item_mean_embeddings = nn.Embedding(args.item_size, args.hidden_size, padding_idx=0)
+        self.item_cov_embeddings = nn.Embedding(args.item_size, args.hidden_size, padding_idx=0)
+        self.position_mean_embeddings = nn.Embedding(args.max_seq_length, args.hidden_size)
+        self.position_cov_embeddings = nn.Embedding(args.max_seq_length, args.hidden_size)
+        self.user_margins = nn.Embedding(args.num_users, 1)
+        self.item_encoder = DistSAEncoder(args)
+        self.LayerNorm = LayerNorm(args.hidden_size, eps=1e-12)
+        self.dropout = nn.Dropout(args.hidden_dropout_prob)
+        self.args = args
+
+        self.apply(self.init_weights)
+
+    def add_position_mean_embedding(self, sequence):
+
+        seq_length = sequence.size(1)
+        position_ids = torch.arange(seq_length, dtype=torch.long, device=sequence.device)
+        position_ids = position_ids.unsqueeze(0).expand_as(sequence)
+        item_embeddings = self.item_mean_embeddings(sequence)
+        position_embeddings = self.position_mean_embeddings(position_ids)
+        sequence_emb = item_embeddings + position_embeddings
+        sequence_emb = self.LayerNorm(sequence_emb)
+        sequence_emb = self.dropout(sequence_emb)
+        elu_act = torch.nn.ELU()
+        sequence_emb = elu_act(sequence_emb)
+
+        return sequence_emb
+
+    def add_position_cov_embedding(self, sequence):
+
+        seq_length = sequence.size(1)
+        position_ids = torch.arange(seq_length, dtype=torch.long, device=sequence.device)
+        position_ids = position_ids.unsqueeze(0).expand_as(sequence)
+        item_embeddings = self.item_cov_embeddings(sequence)
+        position_embeddings = self.position_cov_embeddings(position_ids)
+        sequence_emb = item_embeddings + position_embeddings
+        sequence_emb = self.LayerNorm(sequence_emb)
+        elu_act = torch.nn.ELU()
+        sequence_emb = elu_act(self.dropout(sequence_emb)) + 1
+
+        return sequence_emb
+
+    def finetune(self, input_ids, user_ids):
+
+        attention_mask = (input_ids > 0).long()
+        extended_attention_mask = attention_mask.unsqueeze(1).unsqueeze(2) # torch.int64
+        max_len = attention_mask.size(-1)
+        attn_shape = (1, max_len, max_len)
+        subsequent_mask = torch.triu(torch.ones(attn_shape), diagonal=1) # torch.uint8
+        subsequent_mask = (subsequent_mask == 0).unsqueeze(1)
+        subsequent_mask = subsequent_mask.long()
+
+        if self.args.cuda_condition:
+            subsequent_mask = subsequent_mask.cuda()
+
+        extended_attention_mask = extended_attention_mask * subsequent_mask
+        extended_attention_mask = extended_attention_mask.to(dtype=next(self.parameters()).dtype) # fp16 compatibility
+        extended_attention_mask = (1.0 - extended_attention_mask) * (-2 ** 32 + 1)
+
+        mean_sequence_emb = self.add_position_mean_embedding(input_ids)
+        cov_sequence_emb = self.add_position_cov_embedding(input_ids)
+
+        item_encoded_layers = self.item_encoder(mean_sequence_emb,
+                                                cov_sequence_emb,
+                                                extended_attention_mask,
+                                                output_all_encoded_layers=True)
+
+        mean_sequence_output, cov_sequence_output, att_scores = item_encoded_layers[-1]
+
+        margins = self.user_margins(user_ids)
+        return mean_sequence_output, cov_sequence_output, att_scores, margins
+
+    def init_weights(self, module):
+        """ Initialize the weights.
+        """
+        if isinstance(module, (nn.Linear, nn.Embedding)):
+            # Slightly different from the TF version which uses truncated_normal for initialization
+            # cf https://github.com/pytorch/pytorch/pull/5617
+            module.weight.data.normal_(mean=0.01, std=self.args.initializer_range)
+        elif isinstance(module, LayerNorm):
+            module.bias.data.zero_()
+            module.weight.data.fill_(1.0)
+        if isinstance(module, nn.Linear) and module.bias is not None:
+            module.bias.data.zero_()
+
+
+class DistMeanSAModel(DistSAModel):
+    def __init__(self, args):
+        super(DistMeanSAModel, self).__init__(args)
+        self.item_encoder = DistMeanSAEncoder(args)
+
+
+class RelationAwareSASRecModel(SASRecModel):
+
+    def __init__(self, args, relationships_ind_map):
+        super(RelationAwareSASRecModel, self).__init__(args)
+        self.num_relationships = len(relationships_ind_map)
+        self.item_encoder = RelationAwareSAEncoder(args, self.num_relationships)
+        #self.relationship_embedding = nn.Parameter(torch.rand(self.num_relationships, args.hidden_size, args.hidden_size))
+        #self.relationship_weights = nn.Parameter(torch.rand(self.num_relationships))
+
+    def finetune(self, input_ids, input_rel_seq_masks):
+
+        attention_mask = (input_ids > 0).long()
+        extended_attention_mask = attention_mask.unsqueeze(1).unsqueeze(2) # torch.int64
+        max_len = attention_mask.size(-1)
+        attn_shape = (1, max_len, max_len)
+        subsequent_mask = torch.triu(torch.ones(attn_shape), diagonal=1) # torch.uint8
+        subsequent_mask = (subsequent_mask == 0).unsqueeze(1)
+        subsequent_mask = subsequent_mask.long()
+
+        if self.args.cuda_condition:
+            subsequent_mask = subsequent_mask.cuda()
+            input_rel_seq_masks = input_rel_seq_masks.cuda()
+
+        extended_attention_mask = extended_attention_mask * subsequent_mask
+        extended_attention_mask = extended_attention_mask.to(dtype=next(self.parameters()).dtype) # fp16 compatibility
+        extended_attention_mask = (1.0 - extended_attention_mask) * -10000.0
+
+        input_rel_seq_masks = input_rel_seq_masks.to(dtype=next(self.parameters()).dtype)
+        #for relationship masking, we still need to keep the causality masking
+        #input_rel_seq_masks[extended_attention_mask.expand(-1, self.num_relationships, -1, -1)==0] = 0
+        #input_rel_seq_masks = (1.0 - input_rel_seq_masks) * -10000.0
+
+        sequence_emb = self.add_position_embedding(input_ids)
+
+        item_encoded_layers, relation_embs_layers, relation_weights_layers = self.item_encoder(sequence_emb,
+                                                                                                extended_attention_mask,
+                                                                                                input_rel_seq_masks,
+                                                                                                output_all_encoded_layers=True)
+
+        sequence_output = item_encoded_layers[-1]
+        return sequence_output, sequence_emb, relation_embs_layers, relation_weights_layers
